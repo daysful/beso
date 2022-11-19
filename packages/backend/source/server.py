c93a61acc43238081a5c7ff95664a4cda82eebab
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-19 20:11:07 +0200

    backend > dev: source

--- packages/backend/source/server.py
+++ packages/backend/source/server.py
@@ -1,16 +1,51 @@
 from fastapi import FastAPI
 
+from source.simulation import Simulation
+
 
 
 def generate_server():
     app = FastAPI()
 
+    simulations: dict[str, Simulation] = {}
+
     @app.get("/")
     async def __root__():
-        return {"status": True}
+        return {
+            "status": True,
+            "simulations": simulations,
+        }
 
     @app.post("/start")
     async def __start__():
-        return {"status": False}
+        simulation = Simulation()
+        simulations[simulation.id] = simulation
+
+        return {
+            "status": True,
+            "simulationID": simulation.id,
+        }
+
+    @app.get("/status")
+    async def __status__(simulationID: str):
+        simulation = simulations.get(simulationID)
+
+        return {
+            "status": True,
+            "simulation": simulation,
+        }
+
+    @app.post("/stop")
+    async def __stop__(simulationID: str):
+        simulation = simulations.get(simulationID)
+        if not simulation:
+            return {
+                "status": False,
+            }
+
+        simulation.stop()
+        return {
+            "status": True,
+        }
 
     return app
---
+++ packages/backend/source/simulation.py
@@ -0,0 +1,10 @@
+import uuid
+
+
+
+class Simulation:
+    def __init__(self):
+        self.id = str(uuid.uuid4())
+
+    def stop(self):
+        pass
