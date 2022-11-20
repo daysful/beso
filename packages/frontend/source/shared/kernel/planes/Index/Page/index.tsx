8c8635cfd5b05c98427e24543aaba833eb21747c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-20 22:26:28 +0200

    frontend > setup: package

---
+++ packages/frontend/source/shared/kernel/planes/Index/Page/index.tsx
@@ -0,0 +1,45 @@
+// #region imports
+    // #region libraries
+    import React from 'react';
+
+    import {
+        PluridLink,
+        PluridRouterLink,
+        PluridReactComponent,
+    } from '@plurid/plurid-react';
+    // #endregion libraries
+
+
+    // #region internal
+    import {
+        StyledPage,
+    } from './styled';
+    // #endregion internal
+// #endregion imports
+
+
+
+// #region module
+const Page: PluridReactComponent<{}> = (
+    properties,
+) => {
+    /** properties */
+    // const {
+    //     plurid,
+    // } = properties;
+
+
+    /** render */
+    return (
+        <StyledPage>
+            beteks
+        </StyledPage>
+    );
+}
+// #endregion module
+
+
+
+// #region exports
+export default Page;
+// #endregion exports
