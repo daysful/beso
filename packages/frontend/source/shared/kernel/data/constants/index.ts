a772f5f4862a1619d64eec2d50656f6bf4d4b9c8
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-20 22:35:34 +0200

    frontend > dev: shared/kernel/data/constants

--- packages/frontend/source/shared/kernel/data/constants/index.ts
+++ packages/frontend/source/shared/kernel/data/constants/index.ts
@@ -1,4 +1,6 @@
 // #region module
 export const GRAPHQL_API = 'https://domain.graphql';
 export const STRIPE_API_KEY = 'stripeAPIKey';
+
+export const BETEKS_BACKEND = process.env.BETEKS_BACKEND || 'http://localhost:54567';
 // #endregion module
