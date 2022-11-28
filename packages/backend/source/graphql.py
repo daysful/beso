cbf36314c2dd604450d6ef12a2ee5afaf95c165f
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-28 17:50:31 +0200

    backend > dev: graphql

--- packages/backend/source/graphql.py
+++ packages/backend/source/graphql.py
@@ -23,6 +23,7 @@ class BetseWorldImportFromSVG:
 @strawberry.type
 class BetseWorld:
     id: str
+    name: str
     world_size: float
     cell_radius: float
     cell_height: float
@@ -36,6 +37,31 @@ class BetseWorld:
     use_centers: bool
 
 
+default_betse_world = BetseWorld(
+    id='one',
+    name='one',
+    world_size=150e-6,
+    cell_radius=5.0e-6,
+    cell_height=10.0e-6,
+    cell_spacing=26.0e-9,
+    simulate_single_cell=False,
+    lattice_type='hex',
+    lattice_disorder=0.4,
+    mesh_refinement=BetseWorldMeshRefinement(
+        refine_mesh=True,
+        maximum_steps=10,
+        convergence_threshold=1.5,
+    ),
+    import_from_svg=BetseWorldImportFromSVG(
+        svg_override=False,
+        cells_from_svg='geo/root/root_cells.svg',
+        svg_size=500,
+    ),
+    alpha_shape=0.01,
+    use_centers=False,
+)
+
+
 @strawberry.type
 class Query:
     @strawberry.field
@@ -45,32 +71,24 @@ class Query:
         )
 
     @strawberry.field
-    def betse_world(self) -> BetseWorld:
-        return BetseWorld(
-            id='one',
-            world_size=150e-6,
-            cell_radius=5.0e-6,
-            cell_height=10.0e-6,
-            cell_spacing=26.0e-9,
-            simulate_single_cell=False,
-            lattice_type='hex',
-            lattice_disorder=0.4,
-            mesh_refinement=BetseWorldMeshRefinement(
-                refine_mesh=True,
-                maximum_steps=10,
-                convergence_threshold=1.5,
-            ),
-            import_from_svg=BetseWorldImportFromSVG(
-                svg_override=False,
-                cells_from_svg='geo/root/root_cells.svg',
-                svg_size=500,
-            ),
-            alpha_shape=0.01,
-            use_centers=False,
-        )
+    def betse_world(self, id: str) -> BetseWorld:
+        return default_betse_world
 
 
-schema = strawberry.Schema(query=Query)
+@strawberry.type
+class Mutation:
+    @strawberry.mutation
+    def add_betse_world(self, name: str) -> BetseWorld:
+        betse_world = default_betse_world.__dict__
+        betse_world['name'] = name
+
+        return BetseWorld(**betse_world)
+
+
+schema = strawberry.Schema(
+    query=Query,
+    mutation=Mutation,
+)
 
 
 graphql_app = GraphQL(schema)
