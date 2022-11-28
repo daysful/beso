da36a288087ef480358e52a99a9a719f1056c6dc
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-28 19:34:27 +0200

    backend > dev: graphql

--- packages/backend/source/graphql/mutation/betse.py
+++ packages/backend/source/graphql/mutation/betse.py
@@ -1,14 +1,24 @@
 import strawberry
+from strawberry.tools import create_type
 
 from source.graphql.types.betse import default_betse_world, BetseWorld
 
 
 
-@strawberry.type
-class MutationBetseWorld:
-    @strawberry.mutation
-    def add_betse_world(self, name: str) -> BetseWorld:
-        betse_world = default_betse_world.__dict__
-        betse_world['name'] = name
+def add_betse_world(info, name: str) -> BetseWorld:
+    betse_world = default_betse_world.__dict__
+    betse_world['name'] = name
 
-        return BetseWorld(**betse_world)
+    return BetseWorld(**betse_world)
+
+def remove_betse_world(info, id: str) -> bool:
+    return True
+
+
+MutationBetseWorld = create_type(
+    'MutationBetseWorld',
+    [
+        strawberry.mutation(add_betse_world),
+        strawberry.mutation(remove_betse_world),
+    ],
+)
