f6d1c95bb69eaf10349f79e35998421f2420e180
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-29 22:06:01 +0200

    backend > dev: graphql/mutation

--- packages/backend/source/graphql/mutation/main.py
+++ packages/backend/source/graphql/mutation/main.py
@@ -1,5 +1,6 @@
 from strawberry.tools import merge_types
 
+from .general import MutationGeneral
 from .betse import MutationBetseWorld
 
 
@@ -7,6 +8,7 @@ from .betse import MutationBetseWorld
 Mutation = merge_types(
     'Mutation',
     (
+        MutationGeneral,
         MutationBetseWorld,
     ),
 )
