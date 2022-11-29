60175cc41044abfbd771fc62d31e7bceef197640
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-11-29 16:29:03 +0200

    backend > dev: graphql/query

--- packages/backend/source/graphql/query/general.py
+++ packages/backend/source/graphql/query/general.py
@@ -1,11 +1,16 @@
 import strawberry
 
+from source.constants import allow_user_registration
 from source.graphql.types.general import User
 
 
 
 @strawberry.type
 class QueryUser:
+    @strawberry.field
+    def allowUserRegistration(self) -> bool:
+        return allow_user_registration
+
     @strawberry.field
     def user(self) -> User:
         return User(
