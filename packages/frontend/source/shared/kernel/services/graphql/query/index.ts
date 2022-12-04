9ce99ae011990acafbce90bc191b02b5f71bbe5e
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-12-04 14:59:17 +0200

    frontend > dev: shared/kernel/services/graphql/query

--- packages/frontend/source/shared/kernel/services/graphql/query/index.ts
+++ packages/frontend/source/shared/kernel/services/graphql/query/index.ts
@@ -26,4 +26,118 @@ export const SERVER_USER = gql`
         allowUserRegistration
     }
 `;
+
+
+export const GET_SIMULATIONS = gql`
+    query GetSimulations {
+        getSimulations {
+            id
+            name
+            generatedAt
+            lastRun
+            runs {
+                id
+            }
+        }
+    }
+`;
+
+
+export const GET_WORLDS = gql`
+    query GetWorlds {
+        getWorlds {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_TISSUES = gql`
+    query GetTissues {
+        getTissues {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_GLOBAL_INTERVENTIONS = gql`
+    query GetGlobalInterventions {
+        getGlobalInterventions {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_TARGETED_INTERVENTIONS = gql`
+    query GetTargetedInterventions {
+        getTargetedInterventions {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_MODULATORS = gql`
+    query GetModulators {
+        getModulators {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_NETWOKRS = gql`
+    query GetNetworks {
+        getNetworks {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_BIOMOLECULES = gql`
+    query GetBiomolecules {
+        getBiomolecules {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_REACTIONS = gql`
+    query GetReactions {
+        getReactions {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
+
+
+export const GET_CHANNELS = gql`
+    query GetChannels {
+        getChannels {
+            id
+            name
+            generatedAt
+        }
+    }
+`;
 // #endregion module
