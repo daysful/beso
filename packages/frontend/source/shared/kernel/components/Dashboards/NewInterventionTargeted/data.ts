8d3c83bb4067ea29b8477d1d1a3f8f394ae30058
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-12-08 21:54:29 +0200

    frontend > dev: shared/kernel/components/Dashboards/NewInterventionTargeted

--- packages/frontend/source/shared/kernel/components/Dashboards/NewInterventionTargeted/data.ts
+++ packages/frontend/source/shared/kernel/components/Dashboards/NewInterventionTargeted/data.ts
@@ -2,7 +2,11 @@
     // #region libraries
     import {
         NewEntityField,
-        PrimitiveField
+        PrimitiveField,
+        NumberField,
+        ListField,
+        StringField,
+        BooleanField,
     } from '~kernel-components/NewEntityRenderer/data';
     // #endregion libraries
 // #endregion imports
@@ -59,7 +63,7 @@ const eventRecords: Record<string, PrimitiveField> = {
         type: 'string',
         state: 'modulatorFunction',
         value: '',
-        help: `spatial function: 'gradient_x','gradient_y', 'gradient_r' or 'None'`,
+        help: `spatial function: 'gradient_x','gradient_y', 'gradient_r', 'periodic', or 'None'`,
     },
     applyTo: {
         label: 'apply to',
@@ -69,8 +73,6 @@ const eventRecords: Record<string, PrimitiveField> = {
         help: `name(s) of the tissue profile(s) to apply intervention to`,
     },
 };
-
-const eventFields: PrimitiveField[] = Object.values(eventRecords);
 
 
 export const fields: NewEntityField[] = [
@@ -85,56 +87,246 @@ export const fields: NewEntityField[] = [
         label: 'change Na mem',
         type: 'group',
         state: 'changeNaMem',
-        value: eventFields,
+        value: [
+            eventRecords.eventHappens,
+            {
+                ...eventRecords.changeStart,
+                value: 1.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeFinish,
+                value: 4.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeRate,
+                value: 1.0,
+            } as NumberField,
+            {
+                ...eventRecords.multiplier,
+                value: 10.0,
+            } as NumberField,
+            eventRecords.modulatorFunction,
+            eventRecords.applyTo,
+        ],
         help: 'change the membrane permeability to Na+',
     },
     {
         label: 'change K mem',
         type: 'group',
         state: 'changeKMem',
-        value: eventFields,
+        value: [
+            eventRecords.eventHappens,
+            {
+                ...eventRecords.changeStart,
+                value: 1.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeFinish,
+                value: 2.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeRate,
+                value: 0.5,
+            } as NumberField,
+            {
+                ...eventRecords.multiplier,
+                value: 50.0,
+            } as NumberField,
+            eventRecords.modulatorFunction,
+            eventRecords.applyTo,
+        ],
         help: 'change the membrane permeability to K+',
     },
     {
         label: 'change Cl mem',
         type: 'group',
         state: 'changeClMem',
-        value: eventFields,
+        value: [
+            eventRecords.eventHappens,
+            {
+                ...eventRecords.changeStart,
+                value: 1.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeFinish,
+                value: 4.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeRate,
+                value: 0.5,
+            } as NumberField,
+            {
+                ...eventRecords.multiplier,
+                value: 10.0,
+            } as NumberField,
+            eventRecords.modulatorFunction,
+            eventRecords.applyTo,
+        ],
         help: 'change the membrane permeability to Cl-',
     },
     {
         label: 'change Ca mem',
         type: 'group',
         state: 'changeCaMem',
-        value: eventFields,
+        value: [
+            eventRecords.eventHappens,
+            {
+                ...eventRecords.changeStart,
+                value: 2.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeFinish,
+                value: 8.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeRate,
+                value: 1.0,
+            } as NumberField,
+            {
+                ...eventRecords.multiplier,
+                value: 10.0,
+            } as NumberField,
+            eventRecords.modulatorFunction,
+            {
+                ...eventRecords.applyTo,
+                value: [ 'Base' ],
+            } as ListField,
+        ],
         help: 'change the membrane permeability to Ca2+',
     },
     {
         label: 'apply pressure',
         type: 'group',
         state: 'applyPressure',
-        value: eventFields,
+        value: [
+            eventRecords.eventHappens,
+            {
+                ...eventRecords.changeStart,
+                value: 0.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeFinish,
+                value: 1.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeRate,
+                value: 5.0e-2,
+            } as NumberField,
+            {
+                ...eventRecords.multiplier,
+                value: 100.0,
+            } as NumberField,
+            {
+                ...eventRecords.modulatorFunction,
+                value: 'periodic',
+            } as StringField,
+            eventRecords.applyTo,
+        ],
         help: 'schedule application of mechanical pressure',
     },
     {
         label: 'apply external voltage',
         type: 'group',
         state: 'applyExternalVoltage',
-        value: eventFields,
+        value: [
+            eventRecords.eventHappens,
+            {
+                ...eventRecords.changeStart,
+                value: 0.5,
+                help: 'time [s] at which to begin applying voltage',
+            } as NumberField,
+            {
+                ...eventRecords.changeFinish,
+                value: 1.0,
+                help: 'time [s] at which to cease applying voltage',
+            } as NumberField,
+            {
+                ...eventRecords.changeRate,
+                value: 0.1,
+                help: 'time [s] over which voltage proceeds from background to peak and from peak to background',
+            } as NumberField,
+            {
+                label: 'peak voltage',
+                type: 'number',
+                state: 'peakVoltage',
+                value: 1.0e-3,
+                format: 'scientific',
+                unit: 'V',
+                help: 'max voltage [V] applied by this event',
+            },
+            {
+                label: 'positive voltage boundary',
+                type: 'string',
+                state: 'positiveVoltageBoundary',
+                value: 'top',
+                help: 'env boundary to apply positive voltage ("top", "bottom", "left", or "right")',
+            },
+            {
+                label: 'negative voltage boundary',
+                type: 'string',
+                state: 'negativeVoltageBoundary',
+                value: 'bottom',
+                help: 'env boundary to apply negative voltage ("top", "bottom", "left", or "right")',
+            },
+        ],
         help: `apply an external voltage to two edges of the environmental boundary if the "simulate extracellular spaces" option is enabled`,
     },
     {
         label: 'break ecm junctions',
         type: 'group',
         state: 'breakEcmJunctions',
-        value: eventFields,
+        value: [
+            eventRecords.eventHappens,
+            {
+                ...eventRecords.changeStart,
+                value: 2.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeFinish,
+                value: 7.0,
+            } as NumberField,
+            {
+                ...eventRecords.changeRate,
+                value: 0.5,
+            } as NumberField,
+            {
+                ...eventRecords.multiplier,
+                value: 0.0,
+            } as NumberField,
+            eventRecords.modulatorFunction,
+            eventRecords.applyTo,
+        ],
         help: `break extracellular junctions between cells`,
     },
     {
         label: 'cutting event',
         type: 'group',
         state: 'cuttingEvent',
-        value: eventFields,
+        value: [
+            {
+                ...eventRecords.eventHappens,
+                value: true,
+            } as BooleanField,
+            {
+                ...eventRecords.applyTo,
+                value: [ 'surgery' ],
+            } as ListField,
+            {
+                label: 'break TJ',
+                type: 'boolean',
+                state: 'breakTJ',
+                value: true,
+                help: 'break tight junctions? (otherwise assumes instant barrier healing)',
+            },
+            {
+                label: 'wound TJ',
+                type: 'number',
+                state: 'woundTJ',
+                value: 1.0e-1,
+                format: 'scientific',
+                help: 'how leaky are the tight junctions at the wound (max 1.0)',
+            },
+        ],
         help: `remove all cells selected by one or more cut profiles defined above at an arbitrary simulation time step (ignored unless at least one such profile is defined)`,
     },
 ];
