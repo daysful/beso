ec11dbb2f4fc6d19ec5e2a51b3105f3c0d8498f1
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-12-11 15:47:22 +0200

    frontend > dev: shared/kernel/components/Dashboards/NewTissue

--- packages/frontend/source/shared/kernel/components/Dashboards/NewTissue/data.ts
+++ packages/frontend/source/shared/kernel/components/Dashboards/NewTissue/data.ts
@@ -17,5 +17,144 @@ export const fields: NewEntityField[] = [
         value: 'new tissue',
         required: true,
     },
+    {
+        label: 'insular',
+        type: 'boolean',
+        state: 'insular',
+        value: false,
+        required: true,
+    },
+    {
+        label: 'diffusion constants',
+        type: 'group',
+        state: 'diffusionConstants',
+        value: [
+            {
+                label: 'Dm_Na',
+                type: 'number',
+                state: 'Dm_Na',
+                value: 2.0e-18,
+                format: 'scientific',
+                required: true,
+                unit: 'm2/s',
+                help: `Na+ membrane diffusion constant`,
+            },
+            {
+                label: 'Dm_K',
+                type: 'number',
+                state: 'Dm_K',
+                value: 1.0e-18,
+                format: 'scientific',
+                required: true,
+                unit: 'm2/s',
+                help: `K+ membrane diffusion constant`,
+            },
+            {
+                label: 'Dm_Cl',
+                type: 'number',
+                state: 'Dm_Cl',
+                value: 1.0e-18,
+                format: 'scientific',
+                required: true,
+                unit: 'm2/s',
+                help: `Cl- membrane diffusion constant`,
+            },
+            {
+                label: 'Dm_Ca',
+                type: 'number',
+                state: 'Dm_Ca',
+                value: 1.0e-18,
+                format: 'scientific',
+                required: true,
+                unit: 'm2/s',
+                help: `Ca2+ membrane diffusion constant`,
+            },
+            {
+                label: 'Dm_M',
+                type: 'number',
+                state: 'Dm_M',
+                value: 1.0e-18,
+                format: 'scientific',
+                required: true,
+                unit: 'm2/s',
+                help: `M- membrane diffusion constant`,
+            },
+            {
+                label: 'Dm_P',
+                type: 'number',
+                state: 'Dm_P',
+                value: 0.0,
+                format: 'scientific',
+                required: true,
+                unit: 'm2/s',
+                help: `proteins membrane diffusion constant`,
+            },
+        ],
+        required: true,
+    },
+    {
+        label: 'cell targets',
+        type: 'group',
+        state: 'cellTargets',
+        value: [
+            {
+                label: 'type',
+                type: 'string',
+                state: 'type',
+                value: 'image',
+                required: true,
+                help: `
+                    cell population type as any following string:
+                    * "all", uniformly applying this tissue profile to all cells.
+                    * "color", matching only cells whose cell centres are simple circles with
+                      a fill color (specified by the "color" setting below) of a vector image
+                      (specified by the "cells from svg" setting above).
+                    * "image", matching only cells whose cell centres are pure-black pixels
+                      in a raster image (specified by the "image" setting below).
+                    * "indices", matching only cells whose indices are listed below.
+                    * "percent", matching only a random subset of cells.
+                `,
+            },
+            {
+                label: 'color',
+                type: 'string',
+                state: 'color',
+                value: 'ff0000',
+                required: true,
+                help: `color in hexadecimal format of simple circles within this vector image`,
+            },
+            {
+                label: 'image',
+                type: 'file',
+                state: 'image',
+                required: true,
+            },
+            {
+                label: 'indices',
+                type: 'list',
+                state: 'indices',
+                value: [3, 14, 15, 9, 265],
+                required: true,
+                help: `list of all positive integers uniquely identifying each
+                       cell in this cluster to apply this tissue profile to.
+                       Ignored unless "type: indices" specified above.
+                `
+            },
+            {
+                label: 'percent',
+                type: 'number',
+                state: 'percent',
+                value: 50,
+                format: 'float',
+                required: true,
+                help: `
+                    Percentage of all cells in this cluster to randomly apply this tissue
+                    profile to in the range [0, 100] (e.g., 50 selects 50% of all cells).
+                    Ignored unless "type: random" specified above,
+                `,
+            },
+        ],
+        required: true,
+    },
 ];
 // #endregion module
--- packages/frontend/source/shared/kernel/components/Dashboards/NewTissue/index.tsx
+++ packages/frontend/source/shared/kernel/components/Dashboards/NewTissue/index.tsx
@@ -19,9 +19,10 @@
         DashboardRenderProperties,
     } from '~kernel-components/DashboardsRenderer/data';
 
+    import NewEntityComponent from '~kernel-components/NewEntityComponent';
+
     import {
-        PluridLinkButton,
-        StyledDashboardContainer,
+        PluridDropdown,
     } from '~kernel-services/styled';
 
     import { AppState } from '~kernel-services/state/store';
@@ -32,6 +33,9 @@
 
 
     // #region internal
+    import {
+        fields,
+    } from './data';
     // #endregion internal
 // #endregion imports
 
@@ -75,22 +79,32 @@ const NewTissue: React.FC<NewTissueProperties> = (
 
     // #region render
     return (
-        <StyledDashboardContainer
-            theme={stateGeneralTheme}
-        >
-            <h1>
-                New Tissue
-            </h1>
+        <NewEntityComponent
+            fields={fields}
 
-            <PluridLinkButton
-                text="cancel"
-                atClick={() => {
-                    setFullRenderArea(false);
-                    setRenderView('tissues');
-                }}
-                theme={stateGeneralTheme}
-            />
-        </StyledDashboardContainer>
+            setRenderView={setRenderView}
+            renderViewPath="tissues"
+            setFullRenderArea={setFullRenderArea}
+
+            kind="Tissue"
+            sourceFrom={(
+                <PluridDropdown
+                    selected={'select tissue'}
+                    selectables={[
+                        'none',
+                    ]}
+                    atSelect={(selection) => {
+                        if (typeof selection !== 'string') {
+                            return;
+                        }
+                    }}
+                    style={{
+                        fontSize: '0.9rem',
+                    }}
+                    theme={stateGeneralTheme}
+                />
+            )}
+        />
     );
     // #endregion render
 }
