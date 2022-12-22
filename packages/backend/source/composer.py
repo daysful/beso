dbe339797f7b4151dace15dbcffd808d46e1717c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2022-12-22 21:30:39 +0200

    backend > dev: composer

--- packages/backend/source/composer.py
+++ packages/backend/source/composer.py
@@ -1,62 +1,82 @@
+import json
+import copy
+
 from source.database.collections import Collections
 from source.database.main import get
 
 
 
+def parse(
+    entity: any,
+):
+    if not entity:
+        return
+
+    data = copy.deepcopy(entity)
+    data['data'] = json.loads(data['data'])
+    return data
+
+
+simulation_entities = [
+    {
+        "type": "tissues",
+        "collection": Collections.betseTissues,
+    },
+    {
+        "type": "interventions",
+        "collection": Collections.betseInterventions,
+    },
+    {
+        "type": "functions",
+        "collection": Collections.betseFunctions,
+    },
+    {
+        "type": "networks",
+        "collection": Collections.betseNetworks,
+    },
+    {
+        "type": "biomolecules",
+        "collection": Collections.betseBiomolecules,
+    },
+    {
+        "type": "reactions",
+        "collection": Collections.betseReactions,
+    },
+    {
+        "type": "channels",
+        "collection": Collections.betseChannels,
+    },
+    {
+        "type": "transporters",
+        "collection": Collections.betseTransporters,
+    },
+    {
+        "type": "modulators",
+        "collection": Collections.betseModulators,
+    },
+]
+
+
 def simulation_composer(
     simulation_id: str,
 ):
-    simulation = get(Collections.betseSimulations, simulation_id)
+    simulation = parse(get(Collections.betseSimulations, simulation_id))
     if not simulation:
         return
 
     world = get(Collections.betseWorlds, simulation['world'])
     # get tissues, interventions, etc.
 
-    tissues = []
-    for tissue_id in simulation['tissues']:
-        tissue = get(Collections.betseTissues, tissue_id)
-        tissues.append(tissue)
+    results = {}
 
-    interventions = []
-    for intervention_id in simulation['interventions']:
-        intervention = get(Collections.betseInterventions, intervention_id)
-        interventions.append(intervention)
-
-    functions = []
-    for function_id in simulation['functions']:
-        function = get(Collections.betseFunctions, function_id)
-        functions.append(function)
-
-    networks = []
-    for network_id in simulation['networks']:
-        network = get(Collections.betseFunctions, network_id)
-        networks.append(network)
-
-    biomolecules = []
-    for biomolecule_id in simulation['biomolecules']:
-        biomolecule = get(Collections.betseFunctions, biomolecule_id)
-        biomolecules.append(biomolecule)
-
-    reactions = []
-    for reaction_id in simulation['reactions']:
-        reaction = get(Collections.betseFunctions, reaction_id)
-        reactions.append(reaction)
-
-    channels = []
-    for channel_id in simulation['channels']:
-        channel = get(Collections.betseFunctions, channel_id)
-        channels.append(channel)
-
-    transporters = []
-    for transporter_id in simulation['transporters']:
-        transporter = get(Collections.betseFunctions, transporter_id)
-        transporters.append(transporter)
-
-    modulators = []
-    for modulator_id in simulation['modulators']:
-        modulator = get(Collections.betseFunctions, modulator_id)
-        modulators.append(modulator)
+    for entity in simulation_entities:
+        type = entity['type']
+        results[type] = []
+        for id in simulation[type]:
+            data = get(entity['collection'], id)
+            results[type].append(
+                parse(data),
+            )
 
 
     # write sim_config.yaml with the gathered simulation data
