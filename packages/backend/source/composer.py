4438455edcbf8499a724912c36bbb8182103e490
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2023-01-11 15:17:28 +0200

    backend > dev: composer

--- packages/backend/source/composer.py
+++ packages/backend/source/composer.py
@@ -91,12 +91,60 @@ def compose_simulation(
     simulation_path = betse_copy_data(id)
 
     sim_config = {
+        # SOLVER SETTINGS
+        'solver options': simulation_data['solver_options'],
+
+        # FILE HANDLING
+        'init file saving': {
+            'directory': 'INITS',
+            'worldfile': 'world_1.betse.gz',
+            'file': 'init_1.betse.gz',
+        },
+        'sim file saving': {
+            'directory': 'SIMS',
+            'file': 'sim_1.betse.gz',
+        },
+        'results file saving': {
+            'init directory': 'RESULTS/init_1',
+            'sim directory': 'RESULTS/sim_1',
+        },
+
+        # INITIALIZATION SETTINGS
+        'automatically run initialization': True,
         'init time settings': simulation_data['init_time_settings'],
+
+        # SIMULATION SETTINGS
         'sim time settings': simulation_data['sim_time_settings'],
+
+        # GENERAL OPTIONS
         'general options': simulation_data['general_options'],
+
+        # WORLD OPTIONS
         'world options': world_data,
+
+        # TISSUE PROFILE DESIGNATION
         'tissue profile definition': {},
+
+        # TARGETED INTERVENTIONS
+
+        # MODULATOR FUNCTION PROPERTIES
+
+        # GLOBAL INTERVENTIONS
+
+        # GENERAL NETWORK
+
+        # GENE REGULATORY NETWORK
+
+        # VARIABLE SETTINGS
+        'variable settings': simulation_data['variable_settings'],
+
+        # RESULTS
+        'results options': simulation_data['results_options'],
+
+        # INTERNAL USE ONLY
         'internal parameters': simulation_data['internal_parameters'],
+
+        'version': simulation_data['version'],
     }
 
     sim_config_yaml = yaml.dump(sim_config)
